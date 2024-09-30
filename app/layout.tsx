bdaa61f3c809e1c55a7a57a3d55120bcc1f0bab6
George Parapanissios <gparapanissios@Georges-MBP.station>
2024-09-30 23:48:01 +0300

    add cookie banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 import "./globals.css";
+import CookieBanner from "@/components/cookie-banner";
 
 export const metadata: Metadata = {
   title:
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="el" className="h-full">
-      <body className="h-full flex flex-col">{children}</body>
+      <body className="h-full flex flex-col">
+        {children}
+        <CookieBanner />
+      </body>
       <GoogleAnalytics gaId="G-5LJBVVC7W3" />
     </html>
   );
